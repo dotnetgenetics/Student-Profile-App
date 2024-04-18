0a67ce316e35444f6cda35e9bca45902a120296f
GregoryE <GregoryE@GGSP006450>
2024-04-19 00:48:40 +0800

    Add project files.

---
+++ Demo.StudExer/Demo.StudExer/MarkupExtensions/EmbeddedImage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Demo.StudExer.MarkupExtensions
+{
+   public class EmbeddedImage : IMarkupExtension
+   {
+      public string ResourceId { get; set; }
+      public object ProvideValue(IServiceProvider serviceProvider)
+      {
+         if (string.IsNullOrEmpty(ResourceId))
+            return null;
+
+         return ImageSource.FromResource(ResourceId);
+      }
+   }
+}
